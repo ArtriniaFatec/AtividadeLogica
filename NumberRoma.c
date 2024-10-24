a9a4342474a2d9898ce124cee40947de8e5109ce
ArtriniaFatec <erik.silva39@fatec.sp.gov.br>
2024-10-24 12:27:17 -0300

    primeiro

---
+++ NumberRoma.c
@@ -0,0 +1,13 @@
+int main(){
+    char list = ["I","V","X","L","C","D","M"];
+    char input = [500];
+
+    printf("Enter Number Integer: ");
+    scanf("s%",&input);
+
+    int valor;
+
+    
+
+    return 0;
+}
